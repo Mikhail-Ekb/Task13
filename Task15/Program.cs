5ecd4234ecf1c28f1af124a2c420c453e5bce13e
MIKHAIL <MIKHAIL@DESKTOP-6MU8OHN>
2022-11-22 20:35:21 +0500

    Добавьте файлы проекта.

---
+++ Task15/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\MIKHAIL\Desktop\C#\ИТМО\Task13");
+            Console.WriteLine("==========Список каталогов==========");
+            foreach (var item in dir.GetDirectories())
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine("==Список подкаталогов==");
+                foreach (var it in item.GetDirectories())
+                    Console.WriteLine(it.Name);
+                Console.WriteLine();
+            }
+            Console.WriteLine("==========Список файлов==========");
+            foreach (var item in dir.GetFiles())
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
